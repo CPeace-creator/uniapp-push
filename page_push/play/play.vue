c0b71308de050fdb9233cb1e4a11fa344e2c032a
cjh <949661474@qq.com>
2025-01-13 15:31:05 +0800

    设置抽奖状态样式已经开奖记录

--- page_push/play/play.vue
+++ page_push/play/play.vue
@@ -17,9 +17,26 @@
 					<uv-number-box v-model="formData.number" :min="0")></uv-number-box>
 				</view>
 			</view>
+		</view>
+		<view class="btnGroup">
+			<view class="btn" hover-class="btnHover" hover-start-time="0">
+				<text v-if="false" class="nostart">点击\n抽奖</text>
+				<text v-if="false" class="start">停止</text>
+				<text v-if="true" class="end">已结束</text>
+			</view>
+		</view>
+		<view class="logs">
+			<view class="title">
+				——开奖记录——
+			</view>
+			<view class="content">
+				<uni-list>
+					<uni-list-item v-for="(item,index) in 5" :key="item"
+					clickable="true" showArrow :title="`第${index+1}轮开奖`" note="描述文字" right-text="右侧文本内容"></uni-list-item>
+				</uni-list>
+			</view>
 			
 		</view>
-		
 	</view>
 </template>
 
@@ -53,6 +70,88 @@ const selectChange=(e)=>{
 				flex:1;
 			}
 		}
+	}
+	.btnGroup{
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		padding: 100rpx 0;
+		.btn{
+			width: 260rpx;
+			height: 260rpx;
+			font-size: 46rpx;
+			font-weight: bolder;
+			color: #fff;
+			text{
+				width: 100%;
+				height: 100%;
+				display: flex;
+				justify-content: center;
+				align-items: center;
+				border-radius: 50%;
+				position: relative;
+			}
+			.nostart{
+				background: #ff6a33;
+				&::before{
+					content: "";
+					display: block;
+					width: 110%;
+					height: 110%;
+					border: 1px solid transparent;
+					border-top-color: #ee4626;
+					position: absolute;
+					top: -5%;
+					left: -5%;
+					border-radius: 50%;
+					box-sizing: border-box;
+					animation: rotate 0.7s infinite linear;
+				}
+				&::after{
+					content: "";
+					display: block;
+					width: 120%;
+					height: 120%;
+					border: 1px solid transparent;
+					border-top-color: #ee4626;
+					position: absolute;
+					top: -10%;
+					left: -10%;
+					border-radius: 50%;
+					box-sizing: border-box;
+					animation: rotate 1.2s infinite linear;
+				}
+			}
+			.start{
+				background: #ee4626;	
+			}
+			.end{
+				background: #888;
+			}
+		}
+		.btnHover{
+			transform: scale(0.97);
+		}
+	}
+	.logs{
+		padding: 100rpx 30rpx;
+		.title{
+			text-align: center;
+			font-size: 36rpx;
+			color: #999;
+			font-weight: bolder;
+		}
+		.content{
+			padding: 50rpx 0;
+		}
+	}
+}
+@keyframes rotate {
+	0%{
+		transform: rotate(0);
+	}
+	100%{
+		transform: rotate(360deg);
 	}
 }
 </style>
