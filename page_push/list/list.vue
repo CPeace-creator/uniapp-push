9e70395016240e5fc56f1d89c5f7891562cf93c9
cjh <949661474@qq.com>
2025-01-15 17:01:35 +0800

    渲染抽奖列表数据

--- page_push/list/list.vue
+++ page_push/list/list.vue
@@ -1,4 +1,3 @@
-
 <template>
 	<view class="raffleList">
 		<z-paging v-model="dataList" ref="paging" @query='queryList' :default-page-size="15">
@@ -6,55 +5,65 @@
 				<uni-load-more status="loading"></uni-load-more>
 			</template>
 			<uni-list border-full>
-				<uni-list-item title="列表文字" clickable  note="列表描述信息" showArrow  rightText="右侧文字" :note="`列表${Date.now()}\n已参与10人`"
-				v-for="item in dataList" :key="item" @click="routeTo('/page_push/detail/detail')"></uni-list-item>
+				<uni-list-item title="列表文字" clickable note="列表描述信息" showArrow rightText="右侧文字"
+					:note="`创建于${dayjs(item.create_time).format('YYYY-MM-DD HH:mm:ss')}\n已参与${item.join_count}人`"
+					v-for="item in dataList" :key="item" @click="routeTo('/page_push/detail/detail')"></uni-list-item>
 			</uni-list>
 		</z-paging>
 		<view class="huodong">
 			<button type="warn" @click="routeTo('/page_push/edit/edit')"><text>活动详情</text></button>
-		</view>	
+		</view>
 	</view>
 </template>
-	
+
 <script setup>
-	
-import { routeTo } from '../../utils/utils';
-import {ref} from 'vue'
-const paging= ref(null)
-const content=ref(null)
-const dataList = ref([])
-const addList = ref([])
-const queryList  = (pageNo,pageSize)=>{
-	let data=[]
-	for(let i=0;i<pageSize;i++){
-		let item ={id:i,title:Math.random()}
-		data.push(item)
+	import {
+		routeTo
+	} from '../../utils/utils';
+	import {
+		ref
+	} from 'vue'
+	import DBUtils from '../../utils/dbUtils';
+	import dayjs from 'dayjs';
+	const paging = ref(null)
+	const content = ref(null)
+	const dataList = ref([])
+	const addList = ref([])
+	const queryList = async (pageNo, pageSize) => {
+		const pushData = new DBUtils("push-data");
+		let res = await pushData.query({
+			orderBy: "create_time desc"
+		})
+		if (res.errCode == 0) {
+			paging.value.complete(res.data);
+		} else {
+			paging.value.complete([]);
+		}
 	}
-	paging.value.complete(data);
-}
-const goDetail=()=>{
-	routeTo("/page_push/detail/detail")
-	// uni.navigateTo({
-	// 	url:'/page_push/detail/detail'
-	// })
-}
-const addRaffie=()=>{
-	console.log("点击了");
-}
-const showRight=ref(null)
-const showDrawer=()=>{
-	showRight.value.open()
-}
+	const goDetail = () => {
+		routeTo("/page_push/detail/detail")
+		// uni.navigateTo({
+		// 	url:'/page_push/detail/detail'
+		// })
+	}
+	const addRaffie = () => {
+		console.log("点击了");
+	}
+	const showRight = ref(null)
+	const showDrawer = () => {
+		showRight.value.open()
+	}
 </script>
 
 <style scoped lang="scss">
-	.raffleList{
+	.raffleList {
 		position: relative;
 	}
-	.huodong{
+
+	.huodong {
 		position: absolute;
 		top: 1000rpx;
 		right: 0rpx;
-		
+
 	}
 </style>
--- utils/dbUtils.js
+++ utils/dbUtils.js
@@ -1,12 +1,11 @@
-// dbUtils.js
 export default class DBUtils {
-	// 构造函数，接收表名参数
+
 	constructor(tableName) {
 		this.tableName = tableName;
-		this.db = uniCloud.database(); // 获取云数据库实例
+		this.db = uniCloud.database();
 	}
 
-	// 添加数据
+
 	async add(data) {
 		try {
 			const res = await this.db.collection(this.tableName).add(data);
@@ -17,18 +16,40 @@ export default class DBUtils {
 		}
 	}
 
-	// 查询数据
-	async query(query = {}, options = {}) {
+
+	async query({
+		query = "",
+		where = "",
+		orderBy = "",
+		options = ""
+	}) {
 		try {
-			const res = await this.db.collection(this.tableName).where(query).get(options);
-			return res.data; // 返回查询结果数据
+			let collectionQuery = this.db.collection(this.tableName);
+
+			if (query !== "") {
+				collectionQuery = collectionQuery.where(query);
+			}
+
+			if (orderBy !== "") {
+				collectionQuery = collectionQuery.orderBy(orderBy);
+			}
+
+			if (options !== "") {
+				collectionQuery = collectionQuery.get(options);
+			} else {
+
+				collectionQuery = collectionQuery.get();
+			}
+
+			const res = await collectionQuery;
+			return res.result;
 		} catch (e) {
 			console.error('查询数据失败：', e);
 			throw e;
 		}
 	}
 
-	// 更新数据
+
 	async update(id, data) {
 		try {
 			const res = await this.db.collection(this.tableName).doc(id).update(data);
@@ -39,7 +60,7 @@ export default class DBUtils {
 		}
 	}
 
-	// 删除数据
+
 	async delete(id) {
 		try {
 			const res = await this.db.collection(this.tableName).doc(id).remove();
