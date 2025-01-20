f96699ced74f2abdda9a879cb609a1b931610e60
cjh <949661474@qq.com>
2025-01-20 16:55:02 +0800

    根据抽奖状态进行条件判断

--- page_push/play/play.vue
+++ page_push/play/play.vue
@@ -14,15 +14,18 @@
 					抽取个数
 				</view>
 				<view class="right">
-					<uv-number-box v-model="formData.number" :min="0")></uv-number-box>
+					<view class="group">
+						<uv-number-box :min="0" :max="maxNumber" v-model="formData.number"></uv-number-box>
+						<text>还剩{{maxNumber}}个名额</text>
+					</view>
 				</view>
 			</view>
 		</view>
 		<view class="btnGroup">
-			<view class="btn" hover-class="btnHover" hover-start-time="0">
-				<text v-if="false" class="nostart">点击\n抽奖</text>
-				<text v-if="false" class="start">停止</text>
-				<text v-if="true" class="end">已结束</text>
+			<view class="btn" hover-class="btnHover" hover-start-time="0" @click="handlePush">
+				<text v-if="detail.active_state==1" class="nostart">点击\n抽奖</text>
+				<text v-if="detail.active_state==2" class="start">停止</text>
+				<text v-if="detail.active_state==3" class="end">已结束</text>
 			</view>
 		</view>
 		<view class="logs" v-if="detail.operLogs && detail.operLogs.length>0">
@@ -46,20 +49,27 @@ import {
 		onLoad
 	} from '@dcloudio/uni-app'
 import DBUtils from '../../utils/dbUtils';
+import { showToast } from '../../utils/utils';
+import { getUUId } from '../../utils/tools';
 const id = ref(null)
 onLoad((e)=>{
 	id.value=e.pushId
 	getDetail()
 })	
 const formData=ref({
+	id:getUUId(),
 	aid:"",
 	number:0
 })
 const range=computed(()=>detail.value?.awardsList.map(item=>({value:item.id,text:item.name})))
-const detail=ref()
+const detail=ref({})
+//抽奖个数
+const maxNumber=ref(0)
 //下拉框选择事件
 const selectChange=(e)=>{
 	formData.value.number=0
+	let find = detail.value.awardsList.find(item=>item.id===formData.value.aid)
+	maxNumber.value=find.number;
 }
 
 //获取抽奖内容
@@ -67,6 +77,22 @@ const getDetail=async ()=>{
 	let push=new DBUtils("push-data")
 	let {data:[obj]}=await push.query({query:`_id=="${id.value}"`})
 	detail.value=obj
+}
+
+//点击抽奖
+const handlePush=()=>{
+	if(detail.value.active_state==1){
+		if(!formData.value.aid) return showToast({title:"抽奖选项未选择"})
+		if(formData.value.number==0) return showToast({title:"抽奖数量不能为0"})
+		detail.value.active_state=2
+	}
+	if(detail.value.active_state==2){
+		formData.value.create_date=Date.now()
+		// getDetail()
+	}
+	if(detail.value.active_state==3){
+		return showToast({title:"活动已结束"})
+	}
 }
 </script>
 
@@ -81,6 +107,16 @@ const getDetail=async ()=>{
 			padding: 30rpx;
 			.right{
 				flex:1;
+				.group{
+					display: flex;
+					align-items:center;
+					text{
+						padding-left: 30rpx;
+						color: #666;
+						font-size: 28rpx;
+						font-weight: bolder;
+					}
+				}
 			}
 		}
 	}
@@ -137,6 +173,34 @@ const getDetail=async ()=>{
 			}
 			.start{
 				background: #ee4626;	
+				&::before{
+					content: "";
+					display: block;
+					width: 110%;
+					height: 110%;
+					border: 1px solid transparent;
+					border-top-color: #ee4626;
+					position: absolute;
+					top: -5%;
+					left: -5%;
+					border-radius: 50%;
+					box-sizing: border-box;
+					animation: rotate 0.7s infinite linear;
+				}
+				&::after{
+					content: "";
+					display: block;
+					width: 120%;
+					height: 120%;
+					border: 1px solid transparent;
+					border-top-color: #ee4626;
+					position: absolute;
+					top: -10%;
+					left: -10%;
+					border-radius: 50%;
+					box-sizing: border-box;
+					animation: rotate 1.2s infinite linear;
+				}
 			}
 			.end{
 				background: #888;
