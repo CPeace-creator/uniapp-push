2b16651c31b6a628f181461b608fe808de87fff4
cjh <949661474@qq.com>
2025-01-22 11:40:43 +0800

    初始化init.js

--- App.vue
+++ App.vue
@@ -1,15 +1,10 @@
-<script>
-	export default {
-		onLaunch: function() {
-			console.log('App Launch')
-		},
-		onShow: function() {
-			console.log('App Show')
-		},
-		onHide: function() {
-			console.log('App Hide')
-		}
-	}
+<script setup>
+import uniIdPageInit from '@/uni_modules/uni-id-pages/init.js';
+import {onLaunch} from '@dcloudio/uni-app'
+onLanuch(async()=>{
+	console.log('App Launch')
+	await uniIdPageInit()
+})
 </script>
 
 <style>
